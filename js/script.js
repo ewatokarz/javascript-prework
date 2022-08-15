4e7b5b5dd9d14a8a0aee8216d722d56e10b69c58
Ewa Tokarz <ewa.tokarz@yahoo.com>
2022-08-15 21:30:39 +0200

    assign random numbers to names:kamien, nozyczki, papier

--- js/script.js
+++ js/script.js
@@ -1,6 +1,66 @@
-let computerMove = `kamien`;
+/* let computerMove = `kamien`;
 
 let playerMove = `papier`;
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!!!!!');
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!!!!!');
 
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ulamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1 rowna sie: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokragleniu w dol rowna sie: ' + roundNumber);*/
+
+
+
+
+/*let randomNumber = Math.floor(Math.random() * 9) + 11;*/
+
+
+/*let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+printMessage("wylosowana liczba to: " + randomNumber);
+
+let computerMove = "nieznany ruch";
+
+if(randomNumber == 1) {
+    computerMove = "kamien";
+}
+
+console.log(computerMove);
+
+printMessage("Moj ruch to " + computerMove);
+
+let playerInput = prompt("wybierz swoj ruch 1: kamien, 2: papier, 3: nozyce");
+
+console.log("Gracz wpisal: " + playerInput);
+
+let playerMove = "nieznany ruch";
+
+if(playerInput == '1') {
+    playerMove = 'kamien';
+}
+
+printMessage('Twoj ruch to: ' + playerMove);*/
+
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'nozyczki';
+} else if (randomNumber == 3) {
+    computerMove = 'papier';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
